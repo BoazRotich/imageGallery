91dea5327f3b36b9406f7c332760cd0d182fbff4
BoazRotich <boazkiprotich96@gmail.com>
2024-05-31 20:19:48 +0300

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let scrollcontainer = document.querySelector('.gallery');
+let backbtn = document.getElementById('backbtn');
+let nextbtn = document.getElementById('nextbtn');
+
+scrollcontainer.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    scrollcontainer.scrollLeft += event.deltaY;
+    scrollcontainer.style.scrollBehavior = 'auto';
+});
+
+nextbtn.addEventListener('click', ()=>{
+    scrollcontainer.style.scrollBehavior = 'smooth';
+    scrollcontainer.scrollLeft += 900;
+});
+ backbtn.addEventListener('click', ()=>{
+    scrollcontainer.style.scrollBehavior = 'smooth';
+    scrollcontainer.scrollLeft -= 900;
+ })
